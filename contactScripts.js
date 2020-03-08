e60803d184313a0843878492302845157f946174
WilliamRHarper <Wrharpe1230@gmail.com>
2020-03-08 13:49:31 -0500

    contact me final

--- contactScripts.js
+++ contactScripts.js
@@ -4,3 +4,30 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+/*document.getElementByClassName('field').addEventListener('click', changeColor);
+
+function changeColor() {
+    this.contactMe.backgroundColor = "green";
+    return false;
+}
+
+var elements = document.getElementsByClassName('field');
+var i;
+
+for (i = 0; i < elements.length; ++i) {
+ elements[i].addEventListener('click', changeColor)  
+}
+
+function changeColor() {
+    this.style.backgroundColor = this.getAttribute('data-color');
+}*/
+
+/*const changeColor = () => {
+  
+
+  const fields = document.getElementsByClassName("field")
+    for (i=0; i < fields.length; i++) {
+    fields[i].contactMe.background-color = green
+  }  
+}*/
